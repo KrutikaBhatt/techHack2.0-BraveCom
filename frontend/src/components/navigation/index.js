16391094a14bbfac317f327e0feb798deb09fa9f
KrutikaBhatt <krutikabhatt222@gmail.com>
2021-08-12 09:26:40 +0530

    Added a base navbar

---
+++ frontend/src/components/navigation/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+    Nav,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink
+  } from './styles/navbar';
+  
+export default function Navbar(){
+    return (
+        <>
+        <Nav>
+            <NavLink to='/'>
+            <img src={require('../../images/logo.png')} alt='logo' />
+            </NavLink>
+            <Bars />
+            <NavMenu>
+            <NavLink to='/about' activeStyle>
+                About
+            </NavLink>
+            <NavLink to='/services' activeStyle>
+                Services
+            </NavLink>
+            <NavLink to='/contact-us' activeStyle>
+                Contact Us
+            </NavLink>
+            <NavLink to='/sign-up' activeStyle>
+                Sign Up
+            </NavLink>
+            {/* Second Nav */}
+            {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+            </NavMenu>
+            <NavBtn>
+            <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+            </NavBtn>
+        </Nav>
+        </>
+    )
+}
